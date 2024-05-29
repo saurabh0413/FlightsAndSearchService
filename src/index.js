361ac60dac7cd022c540253babd7a2fdc55156dd
saurabh <saurabhfarkade83@gmail.com>
2024-05-29 19:36:48 +0530

    added city repository

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const { PORT } = require("./config/serverConfig");
 const bodyParser = require("body-parser");
+const { City } = require("./models/index");
 
 const setupAndStartServer = async () => {
   const app = express();
   app.use(bodyParser);
   app.use(bodyParser.urlencoded({ extended: true }));
 
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
     console.log(`server running at ${PORT}`);
+    await City.create({
+      name: "saurabh",
+    });
   });
 };
 
