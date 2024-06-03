7a3e1f0e60bfb661f915fc9066a1ed4374756e59
saurabh <saurabhfarkade83@gmail.com>
2024-06-03 20:44:51 +0530

    Added flight controller and service

--- src/repository/flight-repository.js
+++ src/repository/flight-repository.js
@@ -55,6 +55,20 @@ class FlightRepository {
       throw { error };
     }
   }
+
+  async updateFlights(flightId, data) {
+    try {
+      await Flight.update(data, {
+        where: {
+          id: flightId,
+        },
+      });
+      return true;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
+    }
+  }
 }
 
 module.exports = FlightRepository;
